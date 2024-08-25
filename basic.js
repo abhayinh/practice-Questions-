355515aad296974ef563b59601dcb17b70b97e3f
Abhaysinh Deshmukh <abhaysinhdeshmukh1@gmail.com>
2024-08-25 18:44:55 +0530

    updated

---
+++ basic.js
@@ -0,0 +1,41 @@
+// ################################################
+// JavaScript Program to Convert Kilometers to Miles
+// ###############################################
+
+let kilometer =parseInt(prompt('Enter the kilometer'))
+
+const miles = kilometer*0.621371;
+
+document.write(miles);
+
+
+
+
+
+// ###################################
+// JavaScript Program to Calculate the Area of a Triangle
+// ###########################################
+
+
+// let height= parseInt(prompt("Enter The base"));
+// let base=parseInt(prompt("Enter The base"));
+
+// const Area = (height*base)/2
+// document.write(Area);
+
+
+
+
+
+
+
+// ##############################
+// JavaScript Program to Find the Square Root
+// ###############################
+
+// let number=parseInt(prompt("enter the number"));
+
+// let result=Math.sqrt(number);
+
+// document.write(result);
+
