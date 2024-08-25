00a21068eed417b5a0f757b2b8747782647014c1
Abhaysinh Deshmukh <abhaysinhdeshmukh1@gmail.com>
2024-08-25 19:22:39 +0530

    updated

--- basic.js
+++ basic.js
@@ -1,12 +1,33 @@
+// ##############################################
+// Javascript Program to Generate a Random Number
+// ###############################################
+
+let Random =Math.floor(Math.random()*100)+1;
+document.write(Random);
+
+
+
+// #######################################################
+// Javascript Program to Convert Celsius to Fahrenheit
+// ###############################################
+
+// let calcious = parseInt(prompt('Enter The calcious '));
+
+// const Fahrenheit =calcious*1.8+32;
+// document.write(Fahrenheit);
+
+
+
+
 // ################################################
 // JavaScript Program to Convert Kilometers to Miles
 // ###############################################
 
-let kilometer =parseInt(prompt('Enter the kilometer'))
+// let kilometer =parseInt(prompt('Enter the kilometer'))
 
-const miles = kilometer*0.621371;
+// const miles = kilometer*0.621371;
 
-document.write(miles);
+// document.write(miles);
 
 
 
