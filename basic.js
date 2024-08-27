548514764ba5a64176bde1245879a3bd763d0084
Abhaysinh Deshmukh <abhaysinhdeshmukh1@gmail.com>
2024-08-27 22:52:16 +0530

    All questions added

--- basic.js
+++ basic.js
@@ -1,21 +1,56 @@
+// #####################################################
+// JavaScript Program to Check Prime Number
+// #########################################################
+
+const number = parseInt(prompt("Enter a positive number: "));
+let isPrime = true;
+
+
+if (number === 1) {
+    console.log("1 is neither prime nor composite number.");
+}
+
+
+else if (number > 1) {
+
+   
+    for (let i = 2; i <= number/2; i++) {
+        if (number % i == 0) {
+            isPrime = false;
+            break;
+        }
+    }
+
+    if (isPrime) {
+        console.log(`${number} is a prime number`);
+    } else {
+        console.log(`${number} is a not prime number`);
+    }
+}
+
+
+else {
+    console.log("The number is not a prime number.");
+}
+
 // ########################################################
 // JavaScript Program to Find the Largest Among Three 
 // #########################################################
 
 
-let num1=parseInt(prompt("Enter the first number"));
-let num2=parseInt(prompt("Enter the Secound number"));
-let num3=parseInt(prompt("Enter the third number"));
+// let num1=parseInt(prompt("Enter the first number"));
+// let num2=parseInt(prompt("Enter the Secound number"));
+// let num3=parseInt(prompt("Enter the third number"));
 
-if(num1>num2 && num1>num3){
-    document.write(`${num1} is grater number`);
-}
-else if (num2>num3){
-    document.write(`${num2} is grater number`);
+// if(num1>num2 && num1>num3){
+//     document.write(`${num1} is grater number`);
+// }
+// else if (num2>num3){
+//     document.write(`${num2} is grater number`);
 
-}else{
-    document.write(`${num3} is grater number`)
-}
+// }else{
+//     document.write(`${num3} is grater number`)
+// }
 
 
 
