dce21fb7eeb0a1d957846aac80565c1e57be464c
Abhaysinh Deshmukh <abhaysinhdeshmukh1@gmail.com>
2024-09-26 19:05:24 +0530

    updated

--- basic.js
+++ basic.js
@@ -1,37 +1,58 @@
+// ##################################################
+// Sum of Natural Numbers Using for Loop
+// ##########################################
+
+let number=parseInt(prompt("Enter The number"));
+
+let sum = 0;
+
+
+for(i=0;i<=number;i++){
+    sum +=i
+    // sum= sum+i;
+
+}
+
+console.log(`The sum is ${sum}`);
+
+
+
+
+
 // #####################################################
 // JavaScript Program to Check Prime Number
 // #########################################################
 
-const number = parseInt(prompt("Enter a positive number: "));
-let isPrime = true;
+// const number = parseInt(prompt("Enter a positive number: "));
+// let isPrime = true;
 
 
-if (number === 1) {
-    console.log("1 is neither prime nor composite number.");
-}
+// if (number === 1) {
+//     console.log("1 is neither prime nor composite number.");
+// }
 
 
-else if (number > 1) {
+// else if (number > 1) {
 
    
-    for (let i = 2; i <= number/2; i++) {
-        if (number % i == 0) {
-            isPrime = false;
-            break;
-        }
-    }
+//     for (let i = 2; i <= number/2; i++) {
+//         if (number % i == 0) {
+//             isPrime = false;
+//             break;
+//         }
+//     }
 
-    if (isPrime) {
-        console.log(`${number} is a prime number`);
-    } else {
-        console.log(`${number} is a not prime number`);
-    }
-}
+//     if (isPrime) {
+//         console.log(`${number} is a prime number`);
+//     } else {
+//         console.log(`${number} is a not prime number`);
+//     }
+// }
 
 
-else {
-    console.log("The number is not a prime number.");
-}
+// else {
+//     console.log("The number is not a prime number.");
+// }
 
 // ########################################################
 // JavaScript Program to Find the Largest Among Three 
